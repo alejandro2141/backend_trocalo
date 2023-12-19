a361e8958c9970ee6ae0077b7159445b9850fa4c
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2023-12-19 19:39:07 -0300

    Update server.js

--- server.js
+++ server.js
@@ -51,6 +51,91 @@ const MAX_DAYS_SEARCH  = 60
 */
 
 
+
+
+/******************************************************************************************************** */
+/******************************************************************************************************** */
+/****************                                          ********************************************** */
+/****************      PUBLIC LOGIN   USER  18-12-2023    ********************************************** */
+/****************                                          ********************************************** */
+/******************************************************************************************************** */
+/******************************************************************************************************** */
+
+/******************************************************************************************************** */
+app.route('/public_login_user')
+.post(function (req, res) {
+   // console.log('professional_shutdown_tutorial INPUT : ', req.body );
+   //req.body=sntz_json(req.body,"/professional_shutdown_tutorial")
+// ****** Connect to postgre
+const { Client } = require('pg')
+const client = new Client(conn_data)
+client.connect() 
+
+console.log("REQUEST: "+JSON.stringify(req.body))
+
+
+//let query_reserve =   "INSERT INTO appointment (  date , start_time,  duration,  center_id, confirmation_status, professional_id, patient_doc_id, patient_name,    patient_email, patient_phone1,  patient_age,  app_available, app_status, app_blocked, app_public,  location1, location2, location3, location4, location5, location6,   app_type_home, app_type_center,  app_type_remote, patient_notification_email_reserved , specialty_reserved , patient_address , calendar_id )"   
+
+//************************************  */
+//        INSERT USER_CREATION
+//************************************  */
+let query_insert_user_creation = `INSERT INTO user_creation 
+(names , last_name1, last_name2 , email, phone, id_number, passwd, address_street_name, address_street_number , address_street_apartment , address_location_zone , address_reference) 
+VALUES  
+('${req.body.names}' , '${req.body.last_name1}' , '${req.body.last_name2}' , '${req.body.email}' ,'${req.body.phone}','${req.body.id_number}'
+,'${req.body.passwd}','${req.body.address_street_name}','${req.body.address_street_number}','${req.body.address_street_apartment}','${req.body.address_location_zone}','${req.body.address_reference}'    ) RETURNING * 
+`
+console.log("QUERY Insert User Creation :"+query_insert_user_creation);
+
+const resultado = client.query(query_insert_user_creation, (err, result) => {
+    //res.status(200).send(JSON.stringify(result)) ;
+    if (err) {
+      console.log('/public_register_user ERR:'+err ) ;
+    }
+    else {
+    console.log("public_register_user JSON RESPONSE BODY : "+JSON.stringify(result.rows[0]));
+    }
+    client.end()
+})
+
+
+//************************************  */
+//        INSERT USER
+//************************************  */
+
+const client2 = new Client(conn_data)
+client2.connect() 
+
+console.log("REQUEST: "+JSON.stringify(req.body))
+
+let query_insert_user = `INSERT INTO user_created 
+(names , last_name1, last_name2 , email, phone, id_number, passwd, address_street_name, address_street_number , address_street_apartment , address_location_zone , address_reference) 
+VALUES  
+('${req.body.names}' , '${req.body.last_name1}' , '${req.body.last_name2}' , '${req.body.email}' ,'${req.body.phone}','${req.body.id_number}'
+,'${req.body.passwd}','${req.body.address_street_name}','${req.body.address_street_number}','${req.body.address_street_apartment}','${req.body.address_location_zone}','${req.body.address_reference}'    ) RETURNING * 
+`
+
+console.log("QUERY Insert User  :"+query_insert_user);
+const resultado2 = client2.query(query_insert_user, (err, result) => {
+  //res.status(200).send(JSON.stringify(result)) ;
+  if (err) {
+    console.log('/public_register_user query_insert_user ERR:'+err ) ;
+  }
+  else {
+  console.log("public_register_user query_insert_user JSON RESPONSE BODY : "+JSON.stringify(result));
+  }
+  
+  res.status(200).send(JSON.stringify(result)) ; 
+  client2.end()
+
+})
+
+
+
+ 
+
+})
+
 /******************************************************************************************************** */
 /******************************************************************************************************** */
 /****************                                          ********************************************** */
@@ -58,10 +143,9 @@ const MAX_DAYS_SEARCH  = 60
 /****************                                          ********************************************** */
 /******************************************************************************************************** */
 /******************************************************************************************************** */
-
-// PROFESSIONAL SHUTDOWN TUTORIAL
 // sanitized  31-03-2023 
 // validated 31-03-2023
+/******************************************************************************************************** */
 app.route('/public_register_user')
 .post(function (req, res) {
    // console.log('professional_shutdown_tutorial INPUT : ', req.body );
@@ -73,25 +157,65 @@ client.connect()
 
 console.log("REQUEST: "+JSON.stringify(req.body))
 
+
 //let query_reserve =   "INSERT INTO appointment (  date , start_time,  duration,  center_id, confirmation_status, professional_id, patient_doc_id, patient_name,    patient_email, patient_phone1,  patient_age,  app_available, app_status, app_blocked, app_public,  location1, location2, location3, location4, location5, location6,   app_type_home, app_type_center,  app_type_remote, patient_notification_email_reserved , specialty_reserved , patient_address , calendar_id )"   
-let query_reserve = `INSERT INTO userCreation (name1, name2, lastName1, lastName2 ) VALUES  
-( '${req.body.name1}' , '${req.body.name2}' , '${req.body.lastName1}' , '${req.body.lastName2}' ) RETURNING * 
+
+//************************************  */
+//        INSERT USER_CREATION
+//************************************  */
+let query_insert_user_creation = `INSERT INTO user_creation 
+(names , last_name1, last_name2 , email, phone, id_number, passwd, address_street_name, address_street_number , address_street_apartment , address_location_zone , address_reference) 
+VALUES  
+('${req.body.names}' , '${req.body.last_name1}' , '${req.body.last_name2}' , '${req.body.email}' ,'${req.body.phone}','${req.body.id_number}'
+,'${req.body.passwd}','${req.body.address_street_name}','${req.body.address_street_number}','${req.body.address_street_apartment}','${req.body.address_location_zone}','${req.body.address_reference}'    ) RETURNING * 
 `
+console.log("QUERY Insert User Creation :"+query_insert_user_creation);
 
-console.log(query_reserve);
-
-const resultado = client.query(query_reserve, (err, result) => {
+const resultado = client.query(query_insert_user_creation, (err, result) => {
     //res.status(200).send(JSON.stringify(result)) ;
     if (err) {
       console.log('/public_register_user ERR:'+err ) ;
     }
     else {
-    console.log("public_register_user JSON RESPONSE BODY : "+JSON.stringify(result));
-    res.status(200).send(JSON.stringify(result)) ;  
+    console.log("public_register_user JSON RESPONSE BODY : "+JSON.stringify(result.rows[0]));
     }
     client.end()
 })
 
 
+//************************************  */
+//        INSERT USER
+//************************************  */
+
+const client2 = new Client(conn_data)
+client2.connect() 
+
+console.log("REQUEST: "+JSON.stringify(req.body))
+
+let query_insert_user = `INSERT INTO user_created 
+(names , last_name1, last_name2 , email, phone, id_number, passwd, address_street_name, address_street_number , address_street_apartment , address_location_zone , address_reference) 
+VALUES  
+('${req.body.names}' , '${req.body.last_name1}' , '${req.body.last_name2}' , '${req.body.email}' ,'${req.body.phone}','${req.body.id_number}'
+,'${req.body.passwd}','${req.body.address_street_name}','${req.body.address_street_number}','${req.body.address_street_apartment}','${req.body.address_location_zone}','${req.body.address_reference}'    ) RETURNING * 
+`
+
+console.log("QUERY Insert User  :"+query_insert_user);
+const resultado2 = client2.query(query_insert_user, (err, result) => {
+  //res.status(200).send(JSON.stringify(result)) ;
+  if (err) {
+    console.log('/public_register_user query_insert_user ERR:'+err ) ;
+  }
+  else {
+  console.log("public_register_user query_insert_user JSON RESPONSE BODY : "+JSON.stringify(result));
+  }
+  
+  res.status(200).send(JSON.stringify(result)) ; 
+  client2.end()
+
+})
+
+
+
+ 
 
 })
