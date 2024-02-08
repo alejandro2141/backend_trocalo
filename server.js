b975f3a879fbe6b3696fd9a4c0bec50f8269c835
alejandro2141 <74009893+alejandro2141@users.noreply.github.com>
2024-02-08 19:27:11 -0300

    Update server.js

--- server.js
+++ server.js
@@ -531,10 +531,66 @@ app.route('/public_search_objects')
 
 })
 
+
 /******************************************************************************************************** */
 /******************************************************************************************************** */
 /****************                                          ********************************************** */
-/****************      SEARCH PUBLIC OBJECTS     26-12-2023     ***************************************** */
+/****************      SEARCH PUBLIC OBJECTS BY TEXT
+ *                          26-12-2023                          ***************************************** */
+/****************                                          ********************************************** */
+/******************************************************************************************************** */
+/******************************************************************************************************** */
+// Comments:
+// 
+/******************************************************************************************************** */
+
+app.route('/public_search_objects_by_text')
+.post(function (req, res) {
+
+  const { Client } = require('pg')
+  const client = new Client(conn_data)
+  client.connect() 
+  
+  console.log("/public_search_objects_by_text REQUEST: "+JSON.stringify(req.body))
+ 
+  let json_response = null ;
+  let timestamp= new Date().getTime();
+  let query_search_object = `SELECT * FROM  user_object  WHERE  (deleted_by_owner = FALSE  OR  deleted_by_owner IS  NULL ) AND  ( blocked_due_proposal_accepted = FALSE OR  blocked_due_proposal_accepted IS  NULL )  AND ( UPPER(title) LIKE UPPER('%${req.body.search_text}%') )  ORDER BY id DESC  LIMIT 50  ; 
+  `
+ // console.log("QUERY Insert User  :"+query_insert_img);
+     
+ const resultado = client.query(query_search_object , (err, result) => {
+
+  if (err) 
+  {
+      console.log(' ERROR QUERY = '+query_search_object ) ;
+      console.log(' ERR = '+err ) ;
+  }
+  else 
+  {
+    if (result !=null)
+      {
+      console.log('RESULT public_search_objects'+JSON.stringify(result.rows) ) ;
+      res.status(200).send(JSON.stringify(result.rows) );
+      }
+      else
+      {
+        res.status(200).send( null ) ;
+      }
+  }
+
+  })
+
+})
+
+
+
+
+
+/******************************************************************************************************** */
+/******************************************************************************************************** */
+/****************                                          ********************************************** */
+/****************      GET LAST OBJECTS     26-12-2023     ***************************************** */
 /****************                                          ********************************************** */
 /******************************************************************************************************** */
 /******************************************************************************************************** */
